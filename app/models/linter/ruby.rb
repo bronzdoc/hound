20fd37f04b61af8c77e73d44e74a22e9f7e52353
bronzdoc <lsagastume1990@gmail.com>
2017-08-11 16:40:50 -0600

    wip

--- app/models/linter/ruby.rb
+++ app/models/linter/ruby.rb
@@ -1,8 +1,62 @@
 module Linter
   class Ruby < Base
-    FILE_REGEXP = /.+(\.rb|\.rake)\z/
+    COMMON_RUBY_SOURCE_FILES = [
+      # Include common Ruby source files.
+      "\.builder",
+      "\.fcgi",
+      "\.gemspec",
+      "\.god",
+      "\.jb",
+      "\.jbuilder",
+      "\.mspec",
+      "\.opal",
+      "\.pluginspec",
+      "\.podspec",
+      "\.rabl",
+      "\.rake",
+      "\.rbuild",
+      "\.rbw",
+      "\.rbx",
+      "\.ru",
+      "\.ruby",
+      "\.spec",
+      "\.thor",
+      "\.watchr",
+      "\.irbrc",
+      "\.pryrc",
+      "buildfile",
+      "config.ru",
+      "Appraisals",
+      "Berksfile",
+      "Brewfile",
+      "Buildfile",
+      "Capfile",
+      "Cheffile",
+      "Dangerfile",
+      "Deliverfile",
+      "Fastfile",
+      "Gemfile",
+      "Guardfile",
+      "Jarfile",
+      "Mavenfile",
+      "Podfile",
+      "Puppetfile",
+      "Rakefile",
+      "Snapfile",
+      "Thorfile",
+      "Vagabondfile",
+      "Vagrantfile"
+    ]
+
+    FILE_REGEXP = /.+(\.rb|\.rake|\.gemspec|Gemfile|)\z/
 
     private
+
+    def self.file_regexp
+      COMMON_RUBY_SOURCE_FILES.each do |file|
+        regex = "/.+("
+      end
+    end-
 
     def job_name
       "RubocopReviewJob"
